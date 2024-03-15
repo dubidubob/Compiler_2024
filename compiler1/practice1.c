f8f9041c4af5af32445a8136bdccf92e27a315ce
dubidubob <sildredel@naver.com>
2024-03-15 20:32:06 +0900

    add: main() method

--- compiler1/practice1.c
+++ compiler1/practice1.c
@@ -1,7 +1,3 @@
-/*
-* 
-*/
-
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
@@ -24,3 +20,67 @@ typedef struct HTentry {
 }HTentry;
 
 enum errorTypes { noerror, illsp, illid, overst}; // 에러X, 잘못된 공백, 잘못된 식별자, ST overflow
+typedef enum errorTypes ERRORtypes;
+
+char separators[] = ".,;:?!\t\n";
+
+HTpointer HT[HTsize]; //hash table
+char ST[STsize];
+ERRORtypes err;
+
+FILE* fp; //file pointer
+char input;
+
+int nextid;
+int nextfree;
+
+/*FILE_NAME 파일을 찾아서 첫번째 문자를 가져온다.*/
+void initialize()
+{
+	fp = fopen(FILE_NAME, "r");
+	input = fgetc(fp); //첫 문자
+}
+
+void SkipSeparators()
+{
+	for (int i = 0; i < strlen(separators); i++) {
+		if (input == separators[i]) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void ReadID()
+{
+	//한 자를 받는다.
+		// identifier 중인지 아닌지 설정
+			//현재 nextid=/=nextfree라면 : identifier 상태에 있다면
+				//isLetter인가? 
+					//	ST[nextfree]에 넣고
+					//	nextfree ++
+					//	continue
+				// 아닐 시 separater인가?
+					//	nextfree ++
+				// 아닐 시 illid
+					//	nextfree ++
+			//현재 nextid==nextfree라면: identifier 상태가 아니라면
+				//isLetter인가?
+					//nextfree
+				//skipSeparater인가?
+					//아닐 시 illis
+					//continue
+	// identifier 하나가 완성됐을 시
+			//
+}
+
+int main() 
+{
+	//일단 문서에서 가져오기
+	//이 한 자가 문자라면 ST에 넣고 index 카운팅 시작(separator 처리기에 넣기)
+	//기타라면 영어 시작
+	while (input != EOF) {
+					
+	}
+
+}
